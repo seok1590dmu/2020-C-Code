f03a7cfb7029da8b815fd4be029fcff35023e5a3
seok1590dmu <64784064+seok1590dmu@users.noreply.github.com>
2020-05-05 20:42:46 +0900

    Add files via upload

---
+++ ch11/charray.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void) {
+	//문자 선언과 출력
+	char ch = 'A';
+	printf("%c %d\n", ch, ch);
+
+	//문자열 선언 방법1
+	char java[] = { 'J','A','V','A','\0' };
+	printf("%s\n", java);
+	//문자열 선언 방법2
+	char c[] = "C language";
+	printf("%s\n", c);
+	//문자열 선언 방법3
+	char csharp[5] = "C#";
+	printf("%s\n", csharp);
+
+	printf("%c%c\n", csharp[0], csharp[1]);
+
+	return 0;
+}
---
+++ ch11/lineprint.c
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+
+int main() {
+	char s[100];
+
+	gets(s);
+
+	char* p = s;
+	while (*p) {
+		printf("%c", *p++);
+	}
+	printf("\n");
+
+	return 0;
+}
