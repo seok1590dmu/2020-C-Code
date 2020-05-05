eba8b6ec546098f1c0c683eb34888ecdf24a6671
seok1590dmu <64784064+seok1590dmu@users.noreply.github.com>
2020-05-05 20:43:44 +0900

    Add files via upload

---
+++ ch12/circumference.c
@@ -0,0 +1,5 @@
+extern double PI;
+
+double getCircum(double r) {
+	return 2 * r * PI;
+}
---
+++ ch12/globalvar.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+double getArea(double);
+double getCircum(double);
+
+double PI = 3.14;
+int gi;
+
+int main(void) {
+
+	// 지역변수 선언
+	double r = 5.87;
+	// 전역변수 PI와 같은 이름의 지역변수 선언
+	const double PI = 3.141592;
+
+	printf("면적: %.2f\n", getArea(r));
+	printf("둘레1: %.2f\n", 2 * PI * r);
+	printf("둘레2: %.2f\n", getCircum(r));
+	printf("PI: %f\n", PI);
+	printf("gi: %d\n", gi);
+
+	return 0;
+}
+
+double getArea(double r) {
+	return r * r * PI;
+}
